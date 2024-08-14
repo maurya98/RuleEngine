ee4d8ac17d266840e04311d1d37aa92d61c82729
maurya98 <mauryasaurabh32@gmail.com>
2024-08-15 00:15:10 +0530

    unused import removed

--- src/main/java/com/onecandy/ruleengine/restapi/RuleController.java
+++ src/main/java/com/onecandy/ruleengine/restapi/RuleController.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
